ff76ed0c8a85e1934be341e291ab56b4c15d80b3
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-29 22:14:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
-
   # Define your GET '/' route below and respond with "Hello, World!"
-  require_relative 'config/environment'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
 
